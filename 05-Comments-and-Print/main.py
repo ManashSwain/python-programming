7972cb35e7c4df975ec1c4dd0041bcef8096b426
ManashSwain <manash0802@gmail.com>
2026-08-10 19:42:12 +0530

    Added escape sequence characters

--- 05-Comments-and-Print/main.py
+++ 05-Comments-and-Print/main.py
@@ -5,3 +5,19 @@ This is a
 multi 
 line comment 
 '''
+
+"""
+This is also a 
+multi 
+line comment 
+"""
+
+#Example 1 
+
+# print("This doesnt "execute")
+#Escappe sequence characters
+print("This will \" execute")
+
+#Print arguments
+print("Hello World!")
+print("Welcome", 5 ,sep="~", end= "End of statement")
