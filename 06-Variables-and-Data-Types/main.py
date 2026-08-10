ab17d7f46409642e9a110c39fefcc3f746db5378
ManashSwain <manash0802@gmail.com>
2026-08-10 21:29:35 +0530

    Add datatype codes

---
+++ 06-Variables-and-Data-Types/main.py
@@ -0,0 +1,58 @@
+#variables
+
+# Numeric data types
+
+a = 12 
+b = 8.6
+c = complex(3,6)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+#string
+
+d = "Python programming"
+print(d)
+print(type(d))
+
+#Boolean (True,False)
+
+e = True
+print(e)
+print(type(e))
+
+#NoneType
+
+f = None
+print(f)
+print(type(f))
+
+#list
+
+mylist = [10,20,30]
+print(mylist)
+print(type(mylist))
+
+#tuple
+#Tuple cannot be changed after created
+
+mytuple = ("banana", "apple" , "grapes")
+print(mytuple)
+print(type(mytuple))
+
+#dict
+
+student = {
+    "name": "Manash",
+    "age": 27,
+    "course": "Python"
+}
+
+print(student)
+print(student["name"])
+print(type(student))
