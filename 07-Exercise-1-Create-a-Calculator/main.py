b3b0685b6aa07416408bfbbe7172ebea2469bb1d
ManashSwain <manash0802@gmail.com>
2026-08-11 00:01:20 +0530

    Implement arithmetic operators

---
+++ 07-Exercise-1-Create-a-Calculator/main.py
@@ -0,0 +1,25 @@
+#Operators
+
+a = 15
+b = 6
+
+#Addition 
+print("The Addition of",a,"and",b,"is",(a+b))
+
+#Subtraction
+print("The Subtraction of",a,"and",b,"is",(a-b))
+
+#Multiplication
+print("The Multiplication of",a,"and",b,"is",(a*b))
+
+#Division
+print("The Division of",a,"and",b,"is",(a/b))
+
+#Modulus
+print("The Modulus of",a,"and",b,"is",(a%b))
+
+#Exponential
+print("The Exponential of",a,"and",b,"is",(a**b))
+
+#Floor division
+print("The rounded off value after dividing",a,"and",b,"is",(a//b))
