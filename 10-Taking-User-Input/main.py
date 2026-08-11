8d6ab18b0b1b4a8b3961475957f6b9a3bb3ab216
ManashSwain <manash0802@gmail.com>
2026-08-11 15:31:57 +0530

    Add taking user input

---
+++ 10-Taking-User-Input/main.py
@@ -0,0 +1,16 @@
+a = input("Enter your name : ")
+print(a)
+print(type(a))
+
+#input stores in string
+
+#Method 1 
+x = input("Enter the first number: ")
+y = input("Enter the second number: ")
+
+print(int(x)+int(y))
+
+#Method 2
+p = int(input("Enter the first number: "))
+q = int(input("Enter the second nummber: "))
+print(p+q)
