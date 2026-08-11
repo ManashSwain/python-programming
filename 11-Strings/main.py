da0c7e4e9274fa1629a2669e6514033be5468e02
ManashSwain <manash0802@gmail.com>
2026-08-11 21:19:01 +0530

    Add strings examples

---
+++ 11-Strings/main.py
@@ -0,0 +1,27 @@
+#Strings
+
+name = "Manash"
+print("Hello" + " " +  name)
+
+
+#Accessing letters
+message = "Welcome to python programming"
+print(message[0])
+print(message[1])
+
+#Multiline strings
+#Note : Multiline strings can be enclosed inside tripple single quote or tripe double quote
+
+sentence = """ 
+  This is a 
+  multi line 
+  string
+"""
+print(sentence)
+
+#Loops in string
+
+word= "I want to loop this string"
+
+for i in word:
+    print(i)
