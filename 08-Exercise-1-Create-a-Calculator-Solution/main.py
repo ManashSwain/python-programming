fc5061b24b49b5e6cb75e4b9fcfd6271f5699d9f
ManashSwain <manash0802@gmail.com>
2026-08-11 10:38:20 +0530

    Added calculator solution

---
+++ 08-Exercise-1-Create-a-Calculator-Solution/main.py
@@ -0,0 +1,10 @@
+#Calculator problem solution
+
+a = 16
+b = 5 
+
+print("The Addition of",a,"and",b,"is",(a+b))
+print("The Subtraction of",a,"and",b,"is",(a-b))
+print("The Multiplication of",a,"and",b,"is",(a*b))
+print("The Division of",a,"and",b,"is",(a/b))
+
