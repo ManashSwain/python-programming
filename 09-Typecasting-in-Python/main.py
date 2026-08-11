0a7d4161ab16afd17a3ba83aa24cc981c20ac4b7
ManashSwain <manash0802@gmail.com>
2026-08-11 13:10:05 +0530

    Added typecasting examples

---
+++ 09-Typecasting-in-Python/main.py
@@ -0,0 +1,19 @@
+#Implicit typecasting
+
+a = 7.8 
+b = 5 
+print(a+b)
+
+#Output : 12.8
+# Float + int = Float
+
+print(type(a+b)) #<class 'float'>
+
+#Explicit typecasting
+
+c = "5"
+d = "8"
+print(int(c)+int(d))
+#output : 13
+
+
