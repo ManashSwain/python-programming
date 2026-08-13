10ee49577f5fdfad418d76ed2a27eb32888dd47b
ManashSwain <manash0802@gmail.com>
2026-08-13 23:44:35 +0530

    Add string methods

--- 13-String-Methods/main.py
+++ 13-String-Methods/main.py
@@ -3,3 +3,35 @@
 # upper()
 
 str1 = "manash"
+print(str1.upper()) # MANASH
+
+# lower()
+str2 = "ARTIFICIAL"
+print(str2.lower()) # artificial
+
+# strip()
+str3 = "    Hello world!     "
+print(str3) #    Hello world!
+print(str3.strip()) #Hello world!
+
+# rstrip()
+str4 = "Welcome to python programming !!!!"
+print(str4.rstrip("!")) # Welcome to python programming 
+
+# replace()
+str5 = "Silver spoon"
+print(str5.replace("sp", "m")) # Silver moon
+
+# split()
+str6 = "silver spoon"
+print(str6.split(" ")) # ['silver', 'spoon']
+
+# capitalize()
+
+str7 = "hello world"
+modstr = str7.capitalize()
+print(modstr). # Hello world
+
+str8 = "hello worLD"
+modstr8 = str8.capitalize()
+print(modstr8) # Hello world
