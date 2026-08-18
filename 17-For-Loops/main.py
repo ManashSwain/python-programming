98c18c313cfdac4b2e764f36f6a7b973c47ef0a1
ManashSwain <manash0802@gmail.com>
2026-08-18 22:20:19 +0530

    Add loops example

---
+++ 17-For-Loops/main.py
@@ -0,0 +1,46 @@
+# For loop
+
+name = "Manash"
+
+for i in name:
+    print(i)
+
+# output : 
+# M
+# a
+# n
+# a
+# s
+# h
+
+
+cars = ["Audi", "BMW", "Benz"]
+
+for i in cars:
+    print(i)
+
+# output
+
+# Audi
+# BMW
+# Benz
+
+# Iterating individual strings in list
+
+for car in cars:
+    for i in car:
+        print(i)
+
+# Output 
+# A
+# u
+# d
+# i
+# B
+# M
+# W
+# B
+# e
+# n
+# z
+
