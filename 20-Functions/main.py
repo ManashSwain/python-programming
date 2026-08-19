f7a7be9f479e47620aa45635d1b176686c8bc89e
ManashSwain <manash0802@gmail.com>
2026-08-19 23:52:29 +0530

    Add functions

---
+++ 20-Functions/main.py
@@ -0,0 +1,33 @@
+# Function to add 2 numbers
+
+def add(a,b):
+    sum = a + b 
+    print(sum)
+
+a = 4 
+b = 5
+
+add(a,b)
+
+# Output 
+# 9
+
+# Function to find greater number
+
+def isGreater(a,b):
+    if(a>b):
+        print( a ,"is greater")
+    else:
+        print(b, "b is greater")
+
+c = 7
+d = 4
+isGreater(c,d)
+
+# Output
+# 7 is greater
+
+# pass functionality in functions
+
+def greet():
+    pass
