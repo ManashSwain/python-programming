a7c75f9714791bf21b012e320a1215f7b516821b
ManashSwain <manash0802@gmail.com>
2026-08-21 15:58:19 +0530

    Add Basic list program

---
+++ 22-Introduction-to-Lists/main.py
@@ -0,0 +1,10 @@
+# List
+# Note : List are like arrays
+
+marks = [24,56,77]
+print(marks) # [24, 56, 77]
+
+# Accessing Individual values
+print(marks[0]) # 24
+print(marks[1]) # 56
+print(marks[2]) # 77
