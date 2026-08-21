52c107152aa63c9df1d78ed67c7f86f358b15574
ManashSwain <manash0802@gmail.com>
2026-08-21 20:46:15 +0530

    Add List comprehension with if statement

--- 22-Introduction-to-Lists/main.py
+++ 22-Introduction-to-Lists/main.py
@@ -58,3 +58,9 @@ allNumbers = [1,2,3,4,5,6,7,8,9]
 outputnum = [i*i for i in allNumbers]
 print(outputnum) # [1, 4, 9, 16, 25, 36, 49, 64, 81]
 
+# List comprehension with if statement
+# Syntax new_list = [expression for item in iterable conditionals]
+
+expoutput = [i for i in allNumbers if i%2==0]
+print(expoutput) # [2, 4, 6, 8]
+
