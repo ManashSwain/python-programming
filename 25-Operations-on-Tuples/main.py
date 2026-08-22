322a22a5fe992946185d29d9dc72f1a787d10be1
ManashSwain <manash0802@gmail.com>
2026-08-22 22:28:19 +0530

    Add tuple methods

--- 25-Operations-on-Tuples/main.py
+++ 25-Operations-on-Tuples/main.py
@@ -8,3 +8,16 @@ print(type(converted)) # <class 'list'>
 converted.append(777)
 nums = tuple(converted)
 print(nums) # (12, 23, 45, 87, 54, 777)
+
+
+# Count method
+
+nums2 = (1,1,1,1,1,2,2,2,3,4,)
+print(nums2.count(2)) # 3
+
+# index method
+print(nums2.index(2)) # 5
+
+# index in a particular range
+print(nums2.index(1,3,5)) # 3
+
