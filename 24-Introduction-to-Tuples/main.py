42ef0b8eee319b5e5f91ec4f2ad7b769adb3dbf2
ManashSwain <manash0802@gmail.com>
2026-08-22 16:32:03 +0530

    Add tuple indexing

--- 24-Introduction-to-Tuples/main.py
+++ 24-Introduction-to-Tuples/main.py
@@ -9,3 +9,15 @@ print(type(tup1)) # <class 'tuple'>
 
 print(tup2) # ('Red', 'Green', 'Blue')
 print(type(tup2)) # <class 'tuple'>
+
+# Tuple with different data type
+
+tup3 = ("Rabbit", 56, True, None)
+print(tup3) # ('Rabbit', 56, True, None)
+print(type(tup3)) # <class 'tuple'>
+
+# Tuple Indexing
+
+print(tup1[0]) # 12
+print(tup1[1]) # 24
+print(tup1[2]) # 65
