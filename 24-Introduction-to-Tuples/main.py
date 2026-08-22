c0b7b5411b5642752b5a863066055ac6186c2a64
ManashSwain <manash0802@gmail.com>
2026-08-22 16:17:01 +0530

    Add Basic tuple program

---
+++ 24-Introduction-to-Tuples/main.py
@@ -0,0 +1,11 @@
+# Tuple methods
+# Applying methods on tuple does not change the original tuple
+
+tup1 = (12,24,65)
+tup2 = ("Red","Green","Blue")
+
+print(tup1) # (12, 24, 65)
+print(type(tup1)) # <class 'tuple'>
+
+print(tup2) # ('Red', 'Green', 'Blue')
+print(type(tup2)) # <class 'tuple'>
