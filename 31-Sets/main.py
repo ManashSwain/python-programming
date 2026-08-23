9e1099f72bc0ba6bf0de5cfd56e0a24beb69e8de
ManashSwain <manash0802@gmail.com>
2026-08-23 22:03:07 +0530

    Add Basic set program

---
+++ 31-Sets/main.py
@@ -0,0 +1,6 @@
+# sets 
+# Note : There is no guarantee on the order
+
+items = {"Pickle", 34 , 89.44 , True, None}
+print(items) # {None, True, 34, 'Pickle', 89.44}
+print(type(items)) # <class 'set'>
