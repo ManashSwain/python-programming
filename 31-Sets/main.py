91f2beb9bbc969ce4151f93cd20a2e980a324d69
ManashSwain <manash0802@gmail.com>
2026-08-23 22:08:15 +0530

    Add set programs

--- 31-Sets/main.py
+++ 31-Sets/main.py
@@ -4,3 +4,29 @@
 items = {"Pickle", 34 , 89.44 , True, None}
 print(items) # {None, True, 34, 'Pickle', 89.44}
 print(type(items)) # <class 'set'>
+
+# Accessing set elements
+
+for i in items:
+    print(i)
+
+# Output : 
+# None
+# True
+# 34
+# Pickle
+# 89.44
+
+# Note : Order will be different always
+
+# Quiz
+
+newset = {}
+print(type(newset)) # <class 'dict'>
+
+# Note : Type of empty set is dict
+
+newset = set()
+print(type(newset)) # <class 'set'>
+
+# Note : To create empty set use set() function
