2a9f224f69736e20bca03fe2cbc7a90b9cd1c52e
ManashSwain <manash0802@gmail.com>
2026-08-23 13:16:34 +0530

    Add Fibonacci series example

--- 30-Recursion/main.py
+++ 30-Recursion/main.py
@@ -13,3 +13,31 @@ print(factorial(3)) # 6
 # 1st Iteration : 3 * factorial(2)
 # 2nd iteration : 3 * 2 * factorial(1)
 # 3rd iteration : 3 * 2 * 1 = 6
+
+# Fibonacci series 
+
+# f0 = 0
+# f1 = 1
+# f2 = f1 +f 0
+# f3 = f2 + f1
+
+def fib(n):
+    print("H")
+    if(n==0):
+        return 0
+    if(n==1):
+        return 1
+
+    return fib(n-2) + fib(n-1)
+
+result = fib(5)
+print(result) # 5
+
+# Explanantion
+# 1st Iteration : fib(3) + fib(4)
+# 2nd Iteration : fib(3) : fib(1) + fib(2) , fib(4) : fib(2) + fib(3)
+# 3rd Iteration :  fib(1) = 1 , fib(2) : fib(0) + fib(1) ,fib(2) : fib(0) + fib(1) , fib(3) : fib(1) + fib(2)
+# 4th Iteration :   fib(2) : fib(0)+fib(1)
+# 5th Iteration : 0, 1 
+
+# Note : H will be printed 15 times
