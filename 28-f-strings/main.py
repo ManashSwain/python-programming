daa1b80bbb82a3bb553e8621c6a6b04cb5f3dda1
ManashSwain <manash0802@gmail.com>
2026-08-23 11:43:06 +0530

    Add f string  program

--- 28-f-strings/main.py
+++ 28-f-strings/main.py
@@ -9,3 +9,7 @@ print(updatedsentence) # My name is India and i am from Manash
 
 # Note : If variable order is wrong then the flow also will be wrong
 # Note Old syntax : sentence = "My name is {1} and i am from {0}" 1 and 0 are variable index order
+
+# fstring
+
+print(f"My name is {name} and i am from {country}") # My name is Manash and i am from India
