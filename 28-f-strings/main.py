f3c02f24e9e009ac5f6a340aa8edce273dc4871f
ManashSwain <manash0802@gmail.com>
2026-08-23 12:11:01 +0530

    Add other fstring program

--- 28-f-strings/main.py
+++ 28-f-strings/main.py
@@ -13,3 +13,19 @@ print(updatedsentence) # My name is India and i am from Manash
 # fstring
 
 print(f"My name is {name} and i am from {country}") # My name is Manash and i am from India
+
+# more formatting
+txt = "For only {price:.2f} dollars"
+print(txt.format(price = 49.888888)) # For only 49.89 dollars
+
+# price formatting usinf fstrings
+price = 66.7789
+newtxt = f"For only {price:.2f} dollar"
+print(newtxt) # For only 66.78 dollar
+
+# Other fstring techniques
+
+val = "Geeks"
+print(f"{val} for {val} is a website for {val}") # Geeks for Geeks is a website for Geeks
+
+print(f"value : {2*6}") # value : 12
