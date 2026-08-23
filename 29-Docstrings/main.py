9d075c19cf9ced494079384eaa850bb95c8a3a72
ManashSwain <manash0802@gmail.com>
2026-08-23 12:30:31 +0530

    Add PEP8

--- 29-Docstrings/main.py
+++ 29-Docstrings/main.py
@@ -1,6 +1,7 @@
 # Docstring programs
 # Note : Comments can be ignored but docstings are not ignored 
 # Hint: __ : double underscore
+# Docstring should be written write below the function definition or on the first line within the function 
 
 def square(a):
    '''Function for squaring the number'''
@@ -10,3 +11,6 @@ result = square(5)
 print(result) # 25
 # print the docstring
 print(square.__doc__) # Function for squaring the number
+
+# PEP 8
+# In terminal open python repl and just write import this a poem will be displayed
