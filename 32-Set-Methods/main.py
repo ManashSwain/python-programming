14a25ee9d2e38c0931896f0441e6665b741f680e
ManashSwain <manash0802@gmail.com>
2026-08-23 22:55:48 +0530

    Add Update method

--- 32-Set-Methods/main.py
+++ 32-Set-Methods/main.py
@@ -1,5 +1,15 @@
 # set methods
+# union method
 
 s1 = {1,2,5,6}
 s2 = {2,3,7}
 print(s1.union(s2)) # {1, 2, 3, 5, 6, 7}
+
+# update method
+s3 = {1,2,5,7}
+s4 = {2,3,7}
+print(s3.update(s4))
+print(s3) # {1, 2, 3, 5, 7}
+print(s4) # {2, 3, 7}
+
+# Note : Difference between union and update is union creates a new set where as update modifies the existing set
